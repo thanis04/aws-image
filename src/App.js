2a5ba592bbaa0cbdb98e13b8056c57444619990e
Thanis <mtmorion75@gmail.com>
2024-06-27 18:38:05 +0530

    add hooks for drag ang drop files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import {useDropzone} from 'react-dropzone'
 
 const UserProfiles = () => {
 
@@ -8,7 +9,7 @@ const UserProfiles = () => {
 
   const fetchUserProfiles = async () => {
     await axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
-      console.log(res);
+      // console.log(res);
       setUserProfiles(res.data);
     });
   };
@@ -20,11 +21,52 @@ const UserProfiles = () => {
   return userProfiles.map((userProfile, index) => {
     return(
       <div key={index}>
-      <h1>{userProfile.username}</h1>
-      <p>{userProfile.userProfileId}</p>
+        <br />
+        <br />
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+        <Dropzone {...userProfile}/>
+        <br />
     </div>
     )
   })
+}
+
+function Dropzone({userProfileId}) {
+  const onDrop = useCallback(acceptedFiles => {
+    
+    const file = acceptedFiles[0];
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post(
+      `http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }
+    ).then(() => {
+      console.log("file upload succesfully");
+    }).catch(err => {
+      console.log(err);
+    });
+  });
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the profile image here ...</p> :
+          <p>Drag 'n' drop some profile image here, or click to select profile image</p>
+      }
+    </div>
+  )
 }
 
 function App() {
